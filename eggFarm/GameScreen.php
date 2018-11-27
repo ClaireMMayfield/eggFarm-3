551a9e8b1a404352ef09e036a99e68e47b0aa789
Danielle Nunez <WithEspresso@users.noreply.github.com>
2018-11-26 21:02:09 -0800

    GameScreen from earlier didn't push.

--- eggFarm/GameScreen.php
+++ eggFarm/GameScreen.php
@@ -1,9 +1,28 @@
 <?php
 session_start();
+$username = $_SESSION['username'];
+
+// Get gold and save it to the session.
+$file_name = $username . "_data.txt";
+$user_data = fopen($file_name, "r") or die("Unable to open file");
+$tmp_arry = [];
+while(!feof($user_data)){
+    $line = trim(fgets($user_data));
+    $tmp_arry = explode(":", $line);
+    if(!count($tmp_arry)==2){
+        continue;
+    }
+    if($tmp_arry[0] == "gold")
+    {
+        $gold = $tmp_arry[1];
+        $_SESSION['gold'] = $gold;
+    }
+}
+
 ?>
+
 <?php
 $egg_type = $_GET['egg'];
-
 $configfile = fopen("config.txt", "r") or die("Unable to open file");
 $data = array();
 $temp = array();
@@ -13,7 +32,6 @@ while(!feof($configfile)) {
     if(count($temp) == 1)
         continue;
     $data[$temp[0]] = $temp[1];
-    //echo $tmp_arry[0] ."..".$tmp_arry[1];
 }
 fclose($configfile);
 ?>
@@ -29,17 +47,16 @@ fclose($configfile);
             background-color:lightblue;
             font-family:Courier New;
         }
-
         .nest{
             position:fixed;
             left:-65px;
-            top:550px;
+            top:450px;
             z-index:-1;
         }
         .animalLocation{
             position:fixed;
             left:65px;
-            top:350px;
+            top:325px;
             z-index:1;
         }
         .sell{
@@ -52,19 +69,18 @@ fclose($configfile);
     </style>
 </head>
 
-<body>
-    <h1 id="eggPath"> Egg Path echoed from php is: <?php echo $egg_type; ?> </h1>
-    <h1>Are you ready to play?</h1>
+    <body>
+    <h1>Are you ready to play, <?php echo $username?>?</h1>
     <h2>Press the start button when you are ready!</h2>
     <button id ="startButton">Start</button>
-    <h3>Your Gold: 150</h3>
+    <h3 id="your_gold">Your Gold: <?php echo($_SESSION['gold'])?> </h3>
 
-    <!-- Here I kept your naming scheme with "selected_egg", except now it refers to only the HTML document element. -->
     <span id="tapArea">
-        <img src="eggPlant2_files/<?php echo $_GET['egg'];?>.png" id = "selected_egg" class = "nest1" style="height: 350px;"/>
-        <img class= "nest" src = "nest1.png" alt="Common nest" style="width:500px; height:200px">
+            <img src="eggPlant2_files/<?php echo $_GET['egg'];?>.png" id = "selected_egg" class = "nest1" style="height: 350px;"/>
+            <img class= "nest" src = "nest1.png" alt="Common nest" style="width:500px; height:200px">
     </span>
-
+    <br>
+    <br>
     <br>
     <br>
     <br>
@@ -74,26 +90,33 @@ fclose($configfile);
     <br>
     <br>
     <h3 id="statement">Hi Player 1!</h3>
+    <h3 id="result"> </h3>
 
     <p id="tapCount">Taps: 0</p>
     <p id="timer">Timer: </p>
 
-    <img id="sellA" alt = "Sell" src = "sprite_sell_0.png" class="sell"> </image>
-    <img id = "keepA" alt = "Keep" src = "sprite_keep_0.png" class="keep">  </image>
+    <form name="keepAnimal" action="" onsubmit="" method="post" >
+        <input id="animal_type" type="hidden" name="animal_type" value="">
+        <input id="current_gold" type="hidden" name="current_gold" value="">
+        <input type="submit" value="Save progress">
+    </form>
+
+    <img id="sellA" alt = "Sell" src = "sprite_sell_0.png" class="sell">
+    <img id = "keepA" alt = "Keep" src = "sprite_keep_0.png" class="keep">
 
     <?php
-        $myfile = fopen("config.txt","r") or die ("Unable to open file");
-        $data = array();
-        $temp = array();
-        while(!feof($myfile)){
-            $line = fgets($myfile);
-            $temp = explode(":",$line);
-            if( !isset($temp[1])){
-                $temp[1]= null;
-            }
-            $data[$temp[0]] = $temp[1];
+    $myfile = fopen("config.txt","r") or die ("Unable to open file");
+    $data = array();
+    $temp = array();
+    while(!feof($myfile)){
+        $line = fgets($myfile);
+        $temp = explode(":",$line);
+        if( !isset($temp[1])){
+            $temp[1]= null;
         }
-        fclose($myfile);
+        $data[$temp[0]] = $temp[1];
+    }
+    fclose($myfile);
     ?>
 
     <script type="text/javascript">
@@ -114,27 +137,38 @@ fclose($configfile);
             maxlevel3click: <?php echo $data['maxlevel3click']?>,
             minlevel4click: <?php echo $data['minlevel4click']?>,
             basePath: "../",
-
             animal: "rare_dino"
         };
-
         var Start;
         Start = false;
-
-
         var type_of_egg;
         var selected_egg;
         tapArea.addEventListener("click", addUp, false);
         startButton.addEventListener("click",start,false);
 
 
+        /*
+        * Loads the configuration file for the game
+        */
+        function loadFile(filePath) {
+            var result = null;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", filePath, false);
+            xmlhttp.send();
+            if (xmlhttp.status==200) {
+                result = xmlhttp.responseText;
+            }
+            return result;
+        }
+
+        /*
+        * Starts the game, assigns type of egg for image lookups.
+        * */
         function start(){
             Start = true;
             selected_egg = document.getElementById('selected_egg');
             type_of_egg = <?=json_encode($egg_type)?>;
         }
-
-
         var timer_done = setInterval(function() {
             if(Start == true) {
                 document.getElementById("timer").innerHTML = "Timer: " + Game.time;
@@ -143,7 +177,6 @@ fclose($configfile);
                     document.getElementById("timer").innerHTML = "Timer: " + "OVER!!!";
                     clearInterval(timer_done);
                     Done = true;
-
                     if(x >= 40) {
                         if(x < 50) {
                             selected_egg.src = "eggPlant2_files/" + type_of_egg + "_crack4.png";
@@ -177,34 +210,65 @@ fclose($configfile);
             }
         },100);
 
-
-
-
+        /*
+        * Adding event listeners to the keep and sell buttons.
+        * */
         keepA.addEventListener("click", keepB, false);
         sellA.addEventListener("click", sellB, false);
         document.getElementById('sellA').style.display='none';
         document.getElementById('keepA').style.display='none';
 
+        /*
+        * Function for handling keeping an animal. Removes the "Keep" and "Sell" buttons from the screen.
+        * */
         function keepB(){
-            test.innerHTML = "Good decision! You will collect gold from your animals!";
+            var result = null;
+            var xmlhttp = new XMLHttpRequest();
+            var animal = (Game.animal).replace("sprite_", "");
+            xmlhttp.open("GET", "Update.php?animal='" + animal + "'", true);
+            xmlhttp.send();
+            if (xmlhttp.status == 200) {
+                result = xmlhttp.responseText;
+            }
+            // Create end game elements after the ajax call to save the animal is made.
+            document.getElementById("result").innerHTML = "Good decision! You will collect gold from your animals!";
             document.getElementById('keepA').style.display = 'none';
             document.getElementById('sellA').style.display = 'none';
+            document.getElementById('animal_type').value = Game.animal;
         }
 
+        /*
+        * Function for selling an animal.
+        * */
         function sellB(){
-            test.innerHTML = "Congrats! You'll earn gold for selling your animal!";
+            // Update gold
+            var new_gold = parseInt(Game.gold);
+            new_gold += 500;
+
+            // Ajax call
+            var result = null;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "Update.php?inquiry='gold'&gold='" + new_gold + "'", true);
+            xmlhttp.send();
+            if (xmlhttp.status == 200) {
+                result = xmlhttp.responseText;
+            }
+            document.getElementById("welcome").textContent = "Result is: " + result;
+
+            // Create end game elements after the ajax call to save the new gold amount.
+            document.getElementById("result").innerHTML = "Congrats! You'll earn gold for selling your animal!";
             document.getElementById('keepA').style.display = 'none';
             document.getElementById('sellA').style.display = 'none';
+            document.getElementById('your_gold').value = "Your gold: " + new_gold + " + 500!";
         }
 
+        /*
+        * Function for creating animal html image element and getting number of frames for animating.
+        * */
         var frameNum = 0;
         function createAnimal() {
-
             var x = document.createElement("IMG");
-
             var path_to_animal = Game.animal + "_1.png";
-
-            document.getElementById("eggPath").innerHTML = "Animal's path is: " + path_to_animal;
             x.setAttribute("src", path_to_animal);
             x.setAttribute("id", "animal");
             x.setAttribute("class", "animalLocation");
@@ -212,54 +276,50 @@ fclose($configfile);
             setInterval(frame, 500);
             document.getElementById('sellA').style.display = 'block';
             document.getElementById('keepA').style.display = 'block';
-            if( Game.animal == "sprite_chick"){
+            if(Game.animal == "sprite_chick"){
                 frameNum = 7;
-                }
+            }
             else if(Game.animal == "sprite_lizard"){
                 frameNum = 3;
             }
             else if(Game.animal == "sprite_dino"){
                 frameNum = 3;
-                }
+            }
         }
 
+        /*
+        * Function for animating animal when egg has been hatched.
+        * */
         var curFrame = 0;
-
-
         function frame() {
-
             document.getElementById("animal").src = Game.animal + "_" + curFrame + ".png";
-            document.getElementById("statement").innerHTML = "Yes... You got it!! ";
             curFrame += 1;
             if (curFrame > frameNum)
                 curFrame = 0;
         }
 
+        /*
+        * Function for incrementing click counter while playing game.
+        * */
         var x = 0;
-
-
         function addUp() {
             document.getElementById("tapCount").innerHTML = "Taps: " + x;
-            // document.getElementById("eggPath").innerHTML = "Egg path from javascript is: " + type_of_egg;
             if (!Done && Start) {
                 x = x + 1;
                 if (x > 10 && x < 20) {
                     selected_egg.src = "eggPlant2_files/" + type_of_egg + "_crack1.png";
-                    //document.getElementById("eggPath").innerHTML = "Egg path from javascript is: eggPlant2_files/" + type_of_egg + "_crack1.png";
                     document.getElementById("statement").innerHTML = "Yes... Got it";
                 }
                 if (x > 20 && x <30) {
                     selected_egg.src = "eggPlant2_files/" + type_of_egg + "_crack2.png";
-                    //document.getElementById("eggPath").innerHTML = "Egg path from javascript is: eggPlant2_files/" + type_of_egg + "_crack2.png";
                     document.getElementById("statement").innerHTML = "Yes... Keep tapping!!";
                 }
                 if (x > 30) {
                     selected_egg.src = "eggPlant2_files/" + type_of_egg + "_crack3.png";
-                    //document.getElementById("eggPath").innerHTML = "Egg path from javascript is: eggPlant2_files/" + type_of_egg + "_crack3.png";
                     document.getElementById("statement").innerHTML = "Yes... Getting Close!!";
                 }
             }
         }
     </script>
-</body>
+    </body>
 </html>
