999dc7fa43177f882e498df4d568fc3e9a4dae6c
Danielle Nunez <dnunez@mail.sfsu.edu>
2018-12-02 16:43:16 -0800

    Database now connected to user registration.

--- eggFarm/authorization.php
+++ eggFarm/authorization.php
@@ -3,53 +3,63 @@ user.txt file. !-->
 
 <?php
 session_start();
-error_reporting(0);
+// Database connection credentials.
+$servername = "127.0.0.1:3306";
+$username = "ilmi";
+$password = "Ilmi456!";
+$dbname = "eggfarm";
 
-if(isset($_POST["username"]) && isset($_POST["password"]))
-{
-    //user.txt file
-    $file=fopen("user.txt","r") or die("Unable to open file:".$filename);
-    $finduser = false;
-    while(!feof($file))
-    {
-        $line = fgets($file);
-        $array = explode(":",$line);
-        if(trim($array[0]) == $_POST['username'])
-        {
-            $finduser=true;
-            break;
-        }
+if(isset($_POST["username"]) && isset($_POST["password"])) {
+    $new_username = $_POST["username"];
+    $new_password = $_POST["password"];
+    //$new_username = "dryoon";
+    //$new_password = "Ilmi456!";
+    echo("Attempting to create account for ".$new_username." with password: ".$new_password);
+
+    // Create connection
+    $connection = new mysqli($servername, $username, $password, $dbname);
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    } else {
+        echo("Connection working!");
     }
-    fclose($file);
 
-    if($finduser)
-    {
-        echo $_POST["username"]."\r\n";
+    // Check if username exists already by querying the database for any existing user names.
+    $query = "SELECT username FROM login WHERE username = ?";
+    $result = "";
+
+    // Preparing and executing the sql statement. Results are bound to $result.
+    $sql_statement = $connection->prepare($query);
+    $sql_statement->bind_param("s", $new_username);
+    $sql_statement->execute();
+    $sql_statement->bind_result($result);
+    $sql_statement->fetch();
+    $sql_statement->close();
+
+    // If the results of the database query are equal to the username, redirect to the login page.
+    // Else, create a database entry for the new user. Since the username is a foreign key, we will
+    // simultaneously create a database entry for their user info (gold, animals, etc.).
+    if ($result == $new_username) {
+        echo $new_username . "\r\n";
         echo "is already registered!";
         include 'LoginScreen.html';
         header("Location: LoginScreen.php");
+    } else {
+        // Creates login database entry with new credentials
+        $sql_statement = "INSERT INTO login (username, password) VALUES ('$new_username', '$new_password')";
+        if ($connection -> query($sql_statement) === TRUE) {
+            echo "\nNew login created successfully";
+        } else {
+            echo "Error: " . $sql_statement . "<br>" . $connection->error;
+        }
+        // Creates user_info database entry with 2000 starting gold and 0 animals.
+        $sql_statement = "INSERT INTO user_info (username, gold, dino, lizard, chick) VALUES ('$new_username', '2000', '0', '0', '0')";
+        if ($connection -> query($sql_statement) === TRUE) {
+            echo "\nNew user info entry created successfully";
+        } else {
+            echo "Error: " . $sql_statement . "<br>" . $connection->error;
+        }
     }
-    else
-    {
-        $file = fopen("user.txt", "a+") or die("Unable to open file:".$filename);
-        fputs($file,$_POST["username"].":".$_POST["password"]."\r\n");
-        fclose($file);
-        echo "registered successfully!";
-        include 'LoginScreen.html';
-		header("Location: LoginScreen.php");
-    //new user data file
-    //all new users get 1000 gold pieces
-	$data = 1000;
-    //new users get a new user_data.txt file
-    $dfile = $_POST["username"]."_data.txt";
-    //adds data to file on new lines or dies
-    $datafile = fopen($dfile, "w") or die("Can not open");
-    $userinfo = "username".":".$_POST["username"]."\r\n"."gold".":".$data."\r\n";
-    //adds username and gold amount to files
-    fputs($datafile, $userinfo);
-    //closes file after writing
-    fclose($datafile);
-		//immediately takes users back to the Login page
-    }
+    $connection->close();
 }
 ?>
--- eggFarm/dbConTest.php
+++ eggFarm/dbConTest.php
@@ -1,23 +1,27 @@
 <?php
-$servername = "localhost";
+$servername = "127.0.0.1:3306";
 $username = "ilmi";
 $password = "Ilmi456!";
-$dbname = "myDB";
+$dbname = "eggfarm";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+else {
+    echo("Connection working!");
+}
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+$sql = "INSERT INTO login (username, password) VALUES ('Cmayfield11!', 'Cmay456!')";
+
+if ($conn -> query($sql) === TRUE) {
+    echo "\nNew record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn -> error;
 }
 
 $conn->close();
